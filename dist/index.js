3cafdedd4826130311e6449a6daed3e52612a261
Favas <ricardo.favas@pt.ibm.com>
2023-03-27 20:51:38 +0100

    dist

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,3 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Theme from "./Theme";
-import DemoSite from "./DemoSite";
-import LandingPage from "./LandingPage";
-import "./site.css";
-
-var Site = function Site() {
-  var path = window.location.pathname.replace(/\/$/, "").split("/").slice(-1)[0];
-  return /*#__PURE__*/React.createElement(Theme, null, path === "demo" ? /*#__PURE__*/React.createElement(DemoSite, null) : /*#__PURE__*/React.createElement(LandingPage, null));
-};
-
-ReactDOM.render( /*#__PURE__*/React.createElement(Site, null), document.getElementById("root"));
+import Annotator from "./Annotator";
+export { Annotator };
+export default Annotator;
